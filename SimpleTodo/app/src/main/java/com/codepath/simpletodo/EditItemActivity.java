fdc86466cb8040b42543244162dd7e4a2ddfb17f
Himanshu Masand <masand@seas.upenn.edu>
2015-08-25 23:16:15 -0700

    Adding completed Todo List App

---
+++ SimpleTodo/app/src/main/java/com/codepath/simpletodo/EditItemActivity.java
@@ -0,0 +1,58 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+
+public class EditItemActivity extends ActionBarActivity {
+
+    String itemName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        itemName = getIntent().getStringExtra("itemName");
+        EditText et = (EditText) findViewById(R.id.editText);
+        et.setText(itemName);
+        et.setSelection(et.getText().length());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onSave(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.editText);
+        Intent dataToReturn = new Intent();
+        dataToReturn.putExtra("newItemName", etNewItem.getText().toString());
+        setResult(1, dataToReturn);
+        finish();
+    }
+}
